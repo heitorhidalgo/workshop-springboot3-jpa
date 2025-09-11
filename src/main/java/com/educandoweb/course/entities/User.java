a4d51836fdf3520c3348203c9f0772087702a303
Heitor Hidalgo <heitorhidalgo10@hotmail.com>
2025-09-11 18:52:38 -0300

    JPA repository, dependency injection, database seeding.

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -10,7 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "User")
+@Table(name = "tb_user")
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
